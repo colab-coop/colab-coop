f5d3fc54b7474c69cbdfffc4248419053cb32c91
Karen Menezes <karen.menezes@gmail.com>
2014-10-27 22:11:19 +0530

    CLBWEBSITE-81 nav mobile menu icons

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     //menu: off canvas
     $('.nav-menu-trigger').click(function(event){
         event.stopPropagation();
-        //$(this).toggleClass("nav-icon-active");
+        $(this).children(".nav-icon").html("&#xf00d");
         $('.nav-menu').toggleClass('nav-menu-open');
     });
 
