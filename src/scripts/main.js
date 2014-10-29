97c469f6fc641040a7249420542e9835c6242819
Karen Menezes <karen.menezes@gmail.com>
2014-10-30 02:08:22 +0530

    CLBWEBSITE-82 added pixelate on scroll behaviour

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,28 +1,47 @@
-/*var ctx = demo.getContext('2d'),
-    img = new Image,
-    value = factor.value;
+$(document).ready(function() {
+	var ctx = demo.getContext('2d'),
+	    img = new Image;
+	    //value = factor.value;
 
-img.onload = pixelate;
-img.src = 'http://i.imgur.com/goCYyDB.jpg';
+	img.onload = function() {
+		pixelate(0);
+	};
 
-function pixelate() {
+	img.src = '/assets/img/dummy/panel-bg.jpg';
 
-    /// calculate the factor
-    var fw = (img.width / value)|0,
-        fh = (img.height / value)|0;
-    
-    /// turn off image smoothing (prefixed in some browsers)
-    ctx.imageSmoothingEnabled =
-    ctx.mozImageSmoothingEnabled =
-    ctx.msImageSmoothingEnabled =
-    ctx.webkitImageSmoothingEnabled = false;
-    
-    /// draw mini-version of image
-    ctx.drawImage(img, 0, 0, fw, fh);
-    
-    /// draw the mini-version back up, voila, pixelated
-    ctx.drawImage(demo, 0, 0, fw, fh, 0, 0, img.width, img.height);
-}*/
+	function pixelate(value) {
+
+		if (value === 0) {
+			value = 1;
+		}
+		console.log("value", value);
+	    /// calculate the factor
+	    var fw = (img.width / value)|0,
+	        fh = (img.height / value)|0;
+	    
+	    /// turn off image smoothing (prefixed in some browsers)
+	    ctx.imageSmoothingEnabled =
+	    ctx.mozImageSmoothingEnabled =
+	    ctx.msImageSmoothingEnabled =
+	    ctx.webkitImageSmoothingEnabled = false;
+	    
+	    /// draw mini-version of image
+	    ctx.drawImage(img, 0, 0, fw, fh);
+	    
+	    /// draw the mini-version back up, voila, pixelated
+	    ctx.drawImage(demo, 0, 0, fw, fh, 0, 0, img.width, img.height);
+	}
+
+	$(document).scroll(function() {
+		var scrollTop = $(window).scrollTop();
+		if (scrollTop > 450) {
+			return;
+		}
+		var pixelateValue = parseInt(scrollTop / 12); 
+		pixelate(pixelateValue);
+	});
+});
+
 
 
 /*!
