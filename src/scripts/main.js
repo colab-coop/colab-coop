5a80e70e4039d552f0d73fc6b22385eac235f8e1
Karen Menezes <karen.menezes@gmail.com>
2014-10-30 10:08:47 +0530

    CLBWEBSITE-82 modularised slightly the canvas function for pixelate

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	var ctx = demo.getContext('2d'),
+	var ctx = jsPixelateCanvas.getContext('2d'),
 	    img = new Image;
 	    //value = factor.value;
 
@@ -7,14 +7,17 @@ $(document).ready(function() {
 		pixelate(0);
 	};
 
-	img.src = '/assets/img/dummy/panel-bg.jpg';
+	var imgPixelateId = $('#js-pixelate-scroll').attr('src');
+	img.src = imgPixelateId;
+
+	//img.src = '/assets/img/dummy/panel-bg.jpg';
+
 
 	function pixelate(value) {
 
 		if (value === 0) {
 			value = 1;
 		}
-		console.log("value", value);
 	    /// calculate the factor
 	    var fw = (img.width / value)|0,
 	        fh = (img.height / value)|0;
@@ -29,7 +32,7 @@ $(document).ready(function() {
 	    ctx.drawImage(img, 0, 0, fw, fh);
 	    
 	    /// draw the mini-version back up, voila, pixelated
-	    ctx.drawImage(demo, 0, 0, fw, fh, 0, 0, img.width, img.height);
+	    ctx.drawImage(jsPixelateCanvas, 0, 0, fw, fh, 0, 0, img.width, img.height);
 	}
 
 	$(document).scroll(function() {
@@ -41,6 +44,107 @@ $(document).ready(function() {
 		pixelate(pixelateValue);
 	});
 });
+
+/*
+ * pixelate.js
+ * 43081j
+ * Pixelate images with ease
+ * License: MIT
+ */
+(function(window, $) {
+	var pixelate = function() {
+		var defaults = {
+			value: 0.05,
+			reveal: true,
+			revealonclick: false
+		};
+		var options = arguments[1] || {};
+		var element = this, //arguments[0],
+			elementParent = element.parentNode;
+		if(typeof options !== 'object') {
+			options = { value: parseInt(arguments[1]) };
+		}
+		options = (function() {
+			var opts = {};
+			for(var k in defaults) {
+				if(element.hasAttribute('data-' + k)) {
+					opts[k] = element.getAttribute('data-' + k);
+					continue;
+				}
+				if(k in options) {
+					opts[k] = options[k];
+					continue;
+				}
+				opts[k] = defaults[k];
+			}
+			return opts;
+		})();
+		var display = element.style.display,
+			imgWidth = element.width,
+			imgHeight = element.height,
+			revealed = false;
+		var canv = document.createElement('canvas');
+		canv.width = imgWidth;
+		canv.height = imgHeight;
+		var ctx = canv.getContext('2d');
+		ctx.mozImageSmoothingEnabled = false;
+		ctx.webkitImageSmoothingEnabled = false;
+		ctx.imageSmoothingEnabled = false;
+		var width = imgWidth * options.value,
+			height = imgHeight * options.value;
+		ctx.drawImage(element, 0, 0, width, height);
+		ctx.drawImage(canv, 0, 0, width, height, 0, 0, canv.width, canv.height);
+		element.style.display = 'none';
+		elementParent.insertBefore(canv, element);
+		if(options.revealonclick !== false && options.revealonclick !== 'false') {
+			/*
+			 * Reveal on click
+			 */
+			canv.addEventListener('click', function(e) {
+				revealed = !revealed;
+				if(revealed) {
+					ctx.drawImage(element, 0, 0, imgWidth, imgHeight);
+				} else {
+					ctx.drawImage(element, 0, 0, width, height);
+					ctx.drawImage(canv, 0, 0, width, height, 0, 0, canv.width, canv.height);
+				}
+			});
+		}
+		if(options.reveal !== false && options.reveal !== 'false') {
+			/*
+			 * Reveal on hover
+			 */
+			canv.addEventListener('mouseenter', function(e) {
+				if(revealed) return;
+				ctx.drawImage(element, 0, 0, imgWidth, imgHeight);
+			});
+			canv.addEventListener('mouseleave', function(e) {
+				if(revealed) return;
+				ctx.drawImage(element, 0, 0, width, height);
+				ctx.drawImage(canv, 0, 0, width, height, 0, 0, canv.width, canv.height);
+			});
+		}
+	};
+	window.HTMLImageElement.prototype.pixelate = pixelate;
+	if(typeof $ === 'function') {
+		$.fn.extend({
+			pixelate: function() {
+				return this.each(function() {
+					pixelate.apply(this, arguments);
+				});
+			}
+		});
+	}
+	document.addEventListener('DOMContentLoaded', function(e) {
+		var img = document.querySelectorAll('img[data-pixelate]');
+		for(var i = 0; i < img.length; i++) {
+			img[i].addEventListener('load', function() {
+				this.pixelate();
+			});
+		};
+	});
+})(window, typeof jQuery === 'undefined' ? null : jQuery);
+
 
 
 
@@ -90,6 +194,8 @@ $(document).ready(function(){
 	  	}
 	});
 
+//	$('.img-pixelate-hover').pixelate();
+
     //sticky header
     /*$(window).scroll(function() {
 		if ($(this).scrollTop() > 1){  
@@ -122,6 +228,8 @@ $(document).ready(function(){
     	});
     });
 */
+
+
 
 
 });
