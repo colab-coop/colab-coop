dab5b9580709c25f2ca8314688ebe526d1863e2d
Karen Menezes <karen.menezes@gmail.com>
2014-11-06 10:52:36 +0530

    added jquery extension for headroom.js via browserify

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,6 @@
 var jsPixelateHover = require('./pixelate.js');
 var headroom = require('./headroom.min.js');
+var headroomJQuery = require('./jQuery.headroom.min.js');
 
 //PIXELATE ON SCROLL CODE
 
@@ -126,14 +127,6 @@ $(document).ready(function(){
 	hideTwitterBoxElements();
 
 });
-
-/*!
- * headroom.js v0.7.0 - Give your page some headroom. Hide your header until you need it
- * Copyright (c) 2014 Nick Williams - http://wicky.nillia.ms/headroom.js
- * License: MIT
- */
-
-!function(a){a&&(a.fn.headroom=function(b){return this.each(function(){var c=a(this),d=c.data("headroom"),e="object"==typeof b&&b;e=a.extend(!0,{},Headroom.options,e),d||(d=new Headroom(this,e),d.init(),c.data("headroom",d)),"string"==typeof b&&d[b]()})},a("[data-headroom]").each(function(){var b=a(this);b.headroom(b.data())}))}(window.Zepto||window.jQuery);
 
 //customize twitter feed
 var hideTwitterAttempts = 0;
