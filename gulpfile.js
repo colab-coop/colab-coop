e22286d2cc9459df1dc0053c990ba0b03dd5237b
David Brooks <david@colab.coop>
2014-12-11 12:27:13 -0600

    CLBWEBSITE-133 Add .ico files to generated assets

--- gulpfile.js
+++ gulpfile.js
@@ -190,7 +190,7 @@ gulp.task('scripts', ['browserify'], function() {
 
 // images
 gulp.task('images', function() {
-  return gulp.src(['src/images/**/*.png', 'src/images/**/*.jpg', 'src/images/**/*.svg', 'src/images/**/*.gif'])
+  return gulp.src(['src/images/**/*.png', 'src/images/**/*.jpg', 'src/images/**/*.svg', 'src/images/**/*.gif', 'src/images/**/*.ico'])
     .pipe(
       gulpif(
         isProduction,
