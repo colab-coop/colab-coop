2b9653afc617f0100dbaf986ff347c1e1322018a
Jure Ursic <jure@colab.coop>
2021-02-17 18:55:05 +0100

    Coopernetes

--- config.js
+++ config.js
@@ -1,9 +1,4 @@
-// copy to config.js and adjust as necessary
-
-var src = '/app/src/';
-var dst = '/var/www/html/';
-
 exports.config = {
-  buildDest: dst,
+  buildDest: './build/',
   baseURL: 'http://colab.coop'
 };
